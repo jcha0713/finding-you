b0647426e2d3327543ee4157b2e41d3c38c64e08
jcha0713 <joocha0713@gmail.com>
2020-08-08 02:58:37 +0900

    Fix double star problem

--- canvas.js
+++ canvas.js
@@ -1,21 +1,20 @@
 "use strict";
-
-let you = document.querySelector(".you");
 const universe = document.querySelector(".universe");
 
 let first = true;
 let x = Math.floor(Math.random() * window.innerWidth);
 let y = Math.floor(Math.random() * window.innerHeight);
 
-you.style.width = "40px";
-you.style.height = "40px";
-you.style.borderRadius = "50%";
-you.style.position = "fixed";
-you.style.left = `${x}px`;
-you.style.top = `${y}px`;
+// you.style.width = "40px";
+// you.style.height = "40px";
+// you.style.borderRadius = "50%";
+// you.style.position = "fixed";
+// you.style.left = `${x}px`;
+// you.style.top = `${y}px`;
 
 function found() {
-  you.style.transform = "scale(8)";
+  const star = document.querySelector(".you");
+  star.style.transform = "scale(8)";
   if (first) {
     setTimeout(function () {
       window.alert("FOUND!");
@@ -29,9 +28,14 @@ function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
   cnv.parent(universe);
   let you = createImg("star.png", "star");
+  you.addClass("you");
   you.position(x, y);
   you.style("width", "40px");
   you.style("height", "40px");
+  you.style("border-radius", "50%");
+  you.style("position", "fixed");
+  you.style("left", `${x}px`);
+  you.style("top", `${y}px`);
 }
 
 function windowResized() {

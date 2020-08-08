5604f4ce9c9bb8479229d019a8b5540fac4c561d
jcha0713 <joocha0713@gmail.com>
2020-08-08 11:46:18 +0900

    Disable zoom

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # finding-you
+
+visit: https://jcha0713.github.io/finding-you/
--- canvas.js
+++ canvas.js
@@ -61,3 +61,13 @@ function draw() {
     found();
   }
 }
+
+document.addEventListener(
+  "touchmove",
+  function (event) {
+    if (event.scale !== 1) {
+      event.preventDefault();
+    }
+  },
+  false
+);

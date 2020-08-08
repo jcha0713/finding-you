93d2385f2168f4f6f56f8b32f75d91edfd824570
jcha0713 <joocha0713@gmail.com>
2020-08-08 11:54:58 +0900

    Disable safari pinch zoom

--- canvas.js
+++ canvas.js
@@ -61,13 +61,3 @@ function draw() {
     found();
   }
 }
-
-document.addEventListener(
-  "touchmove",
-  function (event) {
-    if (event.scale !== 1) {
-      event.preventDefault();
-    }
-  },
-  false
-);
